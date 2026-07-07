dec9743695db0d22c7b5d5a8a334924e068954e0
Hendro Paulus Limbong <42108598+hendropl@users.noreply.github.com>
2026-07-07 14:25:52 +0700

    Delete Sistem-Pelayanan-Administrasi-Sukaraja directory

--- Sistem-Pelayanan-Administrasi-Sukaraja/js/supabase.js
+++
@@ -1,8 +0,0 @@
-
-const supabaseUrl = 'https://aqkussjgwhutbzppnaoi.supabase.co';
-
-
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFxa3Vzc2pnd2h1dGJ6cHBuYW9pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMzODM0NDEsImV4cCI6MjA5ODk1OTQ0MX0.yzBZdnVvDBrGFOzEGr86zTM4gcZTGUFVUjJPGs6j_TI';
-
-
-const supabaseClient = window.supabase.createClient(supabaseUrl, supabaseKey);
